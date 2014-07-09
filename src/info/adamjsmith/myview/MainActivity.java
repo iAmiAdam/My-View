5efe340d136aaaefc5ee794f11d1f73281badf97
Adam Smith <adam.smith18@ntlworld.com>
2014-07-09 21:34:30 +0100

    Map Layout and fragment

--- src/info/adamjsmith/myview/MainActivity.java
+++ src/info/adamjsmith/myview/MainActivity.java
@@ -1,7 +1,31 @@
 package info.adamjsmith.myview;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 	
+	private static final int NUM_PAGES = 2;
+	private ViewPager mPager;
+	private PagerAdapter mPagerAdapter;
+	
+	@Override 
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainlayout);
+		
+		mPager = (ViewPager) findViewById(R.id.pager);
+		mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+	}
+	
+	@Override
+	public void onBackPressed() {
+		if (mPager.getCurrentItem() == 0) {
+			super.onBackPressed();
+		} else {
+			mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+		}
+	}
 }
---
+++ src/info/adamjsmith/myview/ViewpointFragment.java
@@ -0,0 +1,18 @@
+package info.adamjsmith.myview;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.MapFragment;
+
+public class ViewpointFragment extends MapFragment{
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.mapfragment, container, false);
+		return rootView;
+	}
+
+}
